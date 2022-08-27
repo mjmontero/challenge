25f2e643b793ba26760413a2edb9381413309c6f
Maria Jose Montero <mari@MacBook-Pro-de-apple.local>
2022-08-27 12:52:12 -0600

    Cleaning up repo

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,7 +2,7 @@ class Tag < ApplicationRecord
   after_save :send_webhook
 
   def send_webhook
-    tag = Tag.select("tag_name, Max(count) as count").first
+    tag = Tag.select("tag_name, Max(count) as count").group(:id).first
     Webhooks::WebhookService.send_request(WEBHOOKSITE_URL, tag.to_json(:only => [ :tag_name, :count ]))
   end
 end
